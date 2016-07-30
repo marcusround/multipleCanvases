f29fc73453c91e5ac9aee3cde48fcac9ffc7cc52
marcusround <marcusround@gmail.com>
2016-07-30 16:00:48 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,7 @@
+function preload() {
+  img = loadImage("assets/tex/paper.jpg")
+}
+
 function setup() {
   createBoxes();
   cnv = createCanvas(300, 300);
@@ -9,15 +13,25 @@ function draw() {
 }
 
 function createBoxes() {
-  var sketch1 = function (p) {
-    p.setup = function(){
-      p.createCanvas(300, 300);
+  for (var i = 0; i < 10; i++){
+    var sketch1 = function (p) {
+      p.setup = function(){
+        p.createCanvas(300, 300);
+        p.imageMode(CENTER);
+      }
+      var n =0;
+      p.draw = function(){
+        p.background(random(64, 128), random(128, 232), random(150, 210)); // cyan
+        f = frameCount%100;
+        if (f == 0 ){
+          n = random(TAU);
+        }
+        p.rotate(n);
+        p.image(img, width/2, height/2);
+      }
     }
-    
-    p.draw = function(){
-      p.background(random(64, 128), random(128, 232), random(150, 210)); // cyan
-    }
-  }
   
   var canvas1 = new p5(sketch1);
-}
+
+  }
+  }
