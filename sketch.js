2734c0e17a36b3d369247e553aeb82bddb12239a
marcusround <marcusround@gmail.com>
2016-07-30 21:01:51 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -152,7 +152,8 @@ function canvasBox (inputText, inputX, inputY, inputWidth, inputHeight, idInput)
     }
     
     p.setup = function() {
-      this.cnv = p.createCanvas(safeOuterWidth, safeOuterHeight).class("canvasBox");
+      this.cnv = p.createCanvas(300, 300);
+      this.cnv.class("canvasBox");
       this.cnv.style("z-index", 99);
     };
     
